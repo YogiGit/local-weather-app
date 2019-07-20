4281f1038cb074794b82daeb848daabc4cc853e3
Yoganand Rajala <ryoganand@gmail.com>
2019-07-20 16:06:30 -0400

    added weather service.

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ICurrentWeather } from '../interfaces';
+
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.scss']
+  styleUrls: ['./current-weather.component.scss'],
 })
 export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.current = {
+      city: 'Alpharetta',
+      country: 'usa',
+      date: new Date(),
+      image: 'assets/img/sunny.svg',
+      tempurature: 72,
+      descrption: 'sunny',
+    };
   }
 
+  ngOnInit() {}
 }
